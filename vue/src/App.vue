e188ffa64499de2d9e60a756a9588b03ee541a5d
no <boooooooo@boo.com>
2023-12-07 14:05:27 -0500

    frontend v2 12/7

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,16 +1,23 @@
 <template>
+        <!-- <div id="hopspotter">
+      <img id="hopspotter" src="src/assets/HopSpotter.png" alt="beerimage">
+    </div> -->
+      <div id="logo">
+      <img id="logo" src="src/assets/HopSpotter.png" alt="beerimage">
+    </div>
   <header>
-    <div id="nav">
+    <div id="topnav" class="topnav">
+      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'BreweryListView' }">Breweries</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'BeerListView', params: { breweryId: 0 } }">Beers</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'BreweryListView' }">News</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'BreweryListView' }">Best Of</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'BreweryListView' }">Reviews</router-link>
     </div>
-    <div> 
-      <img id="logo" src="src/assets/logo_resized.jpg" alt="beerimage">
-    </div>
-    <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+
+    <div id="topnav" class="topnav">
+      <router-link v-bind:to="{ name: 'login' }">Login</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'register' }">Register</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
   </header>
@@ -20,11 +27,13 @@
   </div>
 
   <footer>
-    <div>test</div>
-    <div>test</div>
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link v-bind:to="{ name: 'BreweryListView' }">Breweries</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'BeerListView', params: { breweryId: 0 } }">Beers</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'BreweryListView' }">News</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'BreweryListView' }">Best Of</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'BreweryListView' }">Reviews</router-link>
     </div>
   </footer>
 </template>
@@ -34,21 +43,32 @@
   color: orangered;
 }
 
-header{
+header {
+  margin-left: 15px;
+  margin-right: 15px;
+  margin-bottom: 1px;
+  justify-content: space-between;
   background-color: rgb(0, 0, 0);
   display: flex;
-  align-items: flex-end;
   border-style: none;
   border-color: white;
-  justify-content: space-between;
-  align-items: bottom;
-  height: 100px;
-  
+  height: 90px;
 }
 
-img#logo{
-height: 70px;
-align-items: center;
+footer {
+  margin-top: -20px;
+  background-color: rgb(0, 0, 0);
+  border-color: white;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+img#logo {
+  z-index: 1;
+  height: 80px;
+  align-items: center;
 }
 
 a{
@@ -56,8 +76,47 @@ a{
   text-decoration: none;
 }
 
-div #nav{
-  color: white;
-  font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+ div #topnav{
+  z-index: 0;
+  display: flex;
+  justify-content: space-between;
+  color: gainsboro;
+  align-items: flex-end;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  font-size: smaller;
 }
+
+div #logo{
+  display: flex;
+  translate: -50;
+}
+
+div #nav{
+  display: flex;
+  justify-content:center;
+  align-items: flex-end;
+  color: white;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  font-size: smaller;
+}
+
+#logo {
+  position: absolute;
+left: 50%;
+translate: -40px -2px;
+}
+
+#hopspotter {
+  position: absolute;
+translate: 5px 0px;
+}
+
+img#hopspotter {
+  z-index: -1;
+  height: 75px;
+  align-items: center;
+}
+
+
+
 </style>
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -4,16 +4,56 @@
   <div class="container">
     <img id="taps" src="src/assets/taps.jpg" alt="">
     <div class="centered">
-      <h1>Find your new favorite brewery</h1>
-      <input type="text" id="fname" name="fname" placeholder="Enter an address, neighborhood, city or ZIP code">
-      <div></div>
+      <h1 id="FindYour">Find your new favorite brewery</h1>
+      <input type="text" id="fname" name="fname" placeholder="Enter an Address, City, or Zip">
     </div>
+    <div class="pint1">
+        <img id="taps" src="src/assets/pint.png" alt="">
+      </div>
   </div>
-  <div>
+
+  <hr id="hr1A" class="hr1A">
+  <hr id="hr1B" class="hr1B">
+  <h2 id="LatestReviews">Latest Beer Reviews</h2>
+  <div id="reviews" class="reviews">
+    
+    <div>
+      <img id="review" src="src/assets/logo_resized.jpg" alt="">
+    </div>
+
+    <div>
+      <img id="review" src="src/assets/logo_resized.jpg" alt="">
+    </div>
+
+    <div>
+      <img id="review" src="src/assets/logo_resized.jpg" alt="">
+    </div>
+    <div class="pint2">
+        <img id="taps" src="src/assets/pint.png" alt="">
+  </div>
+  </div>
+
+  <hr id="hr2A" class="hr2A">
+  <hr id="hr2B" class="hr2B">
+
+  <h2 id="LatestReviews">Featured Brewery</h2>
+  <div id="featuredBrewey">
+    <img id="featured" src="src/assets/taps.jpg" alt="">
+
+
+    <div class="pint3">
+        <img id="taps" src="src/assets/pint.png" alt="">
+  </div>
+
+  </div>
+  <hr id="hr2A" class="hr2A">
+  <hr id="hr2B" class="hr2B">
+
+</body>
+<!-- <div>
     <button type="button" v-on:click="$router.push({ name: 'BreweryListView' })" >Breweries</button>
     <button type="button" v-on:click="$router.push({ name: 'BeerListView', params: { breweryId: 0 }})" >Beers</button>
-  </div>
-</body>
+  </div> -->
 </template>
 
 <script>
@@ -22,17 +62,54 @@ export default {
 </script>
 
 <style>
-h1{
+h1#FindYour{
+  font-size: 3.5vw;
   color: white;
   text-shadow: black 2px 2px 5px;
     z-index:1;
 }
+h2{
+  margin-left: 28px;
+  font-size: 28px;
+  color: white;
+  text-shadow: black 2px 2px 5px;
+    z-index:1;
+}
+
+body{
+
+background-image: url("src/assets/woodBG.jpg");
+background-repeat: no-repeat;
+background-attachment: fixed;
+background-position: center;
+background-size: cover;
+box-shadow: 0 0 500px rgba(0,0,0,1) inset;
+
+
+}
+
+
+
 img#taps{
   width: 100%;
-  height: auto;}
+  height: auto;
+}
+
+img#review{
+  width: 50vh;
+  height: auto;
+}
+
+
+img#featured{
+  width: 100%;
+  height: auto;
+}
+
+
 html{
   display: flex;
-justify-content: center;
+  justify-content: center;
   background-color: black;
 }
 
@@ -48,8 +125,64 @@ justify-content: center;
   display: flex;
   flex-direction: column;
   position: absolute;
-  top: 40%;
+  top: 25%;
   transform: translate(-50%, -50%);
   left: 50%;
 }
+
+
+.pint1 {
+  /* border-style: solid; */
+  border-color: white;
+  width: 25px;
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 100%;
+  transform: translate(-50%, -50%);
+  left: 50%;
+}
+
+.pint2 {
+  /* border-style: solid; */
+  border-color: white;
+  width: 25px;
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  transform: translate(-50%, 250px);
+  left: 50%;
+}
+
+.pint3 {
+  /* border-style: solid; */
+  border-color: white;
+  width: 25px;
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  transform: translate(-50%, -50%);
+  left: 50%;
+}
+
+#hr1A {
+  transform: translate(-52%, -400%);
+}
+#hr1B {
+  transform: translate(52%, -900%);
+}
+#hr2A {
+  transform: translate(-52%, -400%);
+}
+#hr2B {
+  transform: translate(52%, -900%);
+}
+
+
+
+  
+  .reviews{
+display: flex;
+justify-content: space-evenly;
+  }
 </style>
