7f28ad52da1fcf564d07cbe4e9c1dffa737f174d
Ian McGreal <imcgreal10@gmail.com>
2023-12-08 16:40:49 -0500

    I have no idea what I am doing in CSS for BreweryDetails

--- vue/src/components/BreweryDetails.vue
+++ vue/src/components/BreweryDetails.vue
@@ -1,140 +1,96 @@
 <template>
+
   <div class="BreweryDetails">
-  <div id="one" class="BreweryDetails">      
-    <div id="BreweryImage" class="BreweryImage">
-    <img id="BreweryImage" class="BreweryImage" :src=brewery.brewery_main_img alt="">
-  </div>
-</div>
-  <div id="two" class="BreweryDetails">2</div>
 
+    <div id="one" class="BreweryDetails">   
 
-
-</div>
-
-
-
-  <div class="">
-    <div>
-    <h1>{{ brewery.brewery_name }}</h1>
-  </div>
-    <!-- <div class="actions">
-      
-    </div> -->
-    
-
-
-    <div class="history">
-      <label>Brewery History:</label>&nbsp;
-      <span>{{ brewery.history }}</span>
-    </div>
-
-    <!-- <div class="hours">
-      <label>Hours of Operation</label>&nbsp;
-      <div class="week">
-        <div>
-          Sunday: {{ this.convertTime(brewery.brewery_hours.sunday_open) }} - {{ this.convertTime(brewery.brewery_hours.sunday_close) }}
-        </div>
-        <div>
-          Monday: {{ this.convertTime(brewery.brewery_hours.monday_open) }} - {{ this.convertTime(brewery.brewery_hours.monday_close) }}
-        </div>
-        <div>
-          Tuesday: {{ this.convertTime(brewery.brewery_hours.tuesday_open) }} - {{ this.convertTime(brewery.brewery_hours.tuesday_close) }}
-        </div>
-        <div>
-          Wednesday: {{ this.convertTime(brewery.brewery_hours.wednesday_open) }} - {{ this.convertTime(brewery.brewery_hours.wednesday_close) }}
-        </div>
-        <div>
-          Thursday: {{ this.convertTime(brewery.brewery_hours.thursday_open) }} - {{ this.convertTime(brewery.brewery_hours.thursday_close) }}
-        </div>
-        <div>
-          Friday: {{ this.convertTime(brewery.brewery_hours.friday_open) }} - {{ this.convertTime(brewery.brewery_hours.friday_close) }}
-        </div>
-        <div>
-          Saturday: {{ this.convertTime(brewery.brewery_hours.saturday_open) }} - {{ this.convertTime(brewery.brewery_hours.saturday_close) }}
-        </div>
+      <div id="BreweryImage" class="BreweryImage">
+        <img id="BreweryImage" class="BreweryImage" :src=brewery.brewery_main_img alt="">
       </div>
-    </div> -->
-    <div class="hours">
-      <label>Hours of Operation</label>
-      <table id="week">
-        <tr>
-          <th>Day</th>
-          <th>Open</th>
-          <th>Close</th>
-        </tr>
-        <tr>
-          <td>Sunday</td>
-          <td>{{ this.convertTime(brewery.brewery_hours.sunday_open) }}</td>
-          <td>{{ this.convertTime(brewery.brewery_hours.sunday_close) }}</td>
-        </tr>
-        <tr>
-          <td>Monday</td>
-          <td>{{ this.convertTime(brewery.brewery_hours.monday_open) }}</td>
-          <td>{{ this.convertTime(brewery.brewery_hours.monday_close) }}</td>
-        </tr>
-        <tr>
-          <td>Tuesday</td>
-          <td>{{ this.convertTime(brewery.brewery_hours.tuesday_open) }}</td>
-          <td>{{ this.convertTime(brewery.brewery_hours.tuesday_close) }}</td>
-        </tr>
-        <tr>
-          <td>Wednesday</td>
-          <td>{{ this.convertTime(brewery.brewery_hours.wednesday_open) }}</td>
-          <td>{{ this.convertTime(brewery.brewery_hours.wednesday_close) }}</td>
-        </tr>
-        <tr>
-          <td>Thursday</td>
-          <td>{{ this.convertTime(brewery.brewery_hours.thursday_open) }}</td>
-          <td>{{ this.convertTime(brewery.brewery_hours.thursday_close) }}</td>
-        </tr>
-        <tr>
-          <td>Friday</td>
-          <td>{{ this.convertTime(brewery.brewery_hours.friday_open) }}</td>
-          <td>{{ this.convertTime(brewery.brewery_hours.friday_close) }}</td>
-        </tr>
-        <tr>
-          <td>Saturday</td>
-          <td>{{ this.convertTime(brewery.brewery_hours.saturday_open) }}</td>
-          <td>{{ this.convertTime(brewery.brewery_hours.saturday_close) }}</td>
-        </tr>
-      </table>
+
     </div>
 
-    <div class="contact-info">
-      <label>Contact Info</label>
-      <table>
-        <tr>
-          <th>Address</th>
-          <th>Phone Number</th>
-          <th>Email Address</th>
-        </tr>
-        <tr>
-          <td>{{ brewery.brewery_contact.brewery_address }}</td>
-          <td>{{ brewery.brewery_contact.phone }}</td>
-          <td>{{ brewery.brewery_contact.email }}</td>
-        </tr>
-      </table>
-    </div>
+    <div id="two" class="BreweryDetails">
 
+      <div class="name">
+        <h1>{{ brewery.brewery_name }}</h1>
+      </div>
 
-        <!-- <div>
-          <div>
-            Email: <span>{{ brewery.brewery_contact.email }}</span>
-          </div>
-          <div>
-            Phone: <span>{{ brewery.brewery_contact.phone }}</span>
-          </div>
-          <div>
-            Address: <span>{{ brewery.brewery_contact.brewery_address }}</span>
-          </div>
-        </div> -->
-    
+      <div class="history">
+        <label>Brewery History:</label>&nbsp;
+        <span>{{ brewery.history }}</span>
+      </div>
 
-    <div class="beers">
-        <button class="btn-see-beers" v-on:click="$router.push({ name: 'BeerListView', params: { breweryId: brewery.brewery_id }})">See Beers</button>
-    </div>
-    <div>
-        <button class="btn-edit-contact" v-on:click="editContact">Edit Contact Info</button>
+      <div class="contact-info">
+        <label>Contact Info</label>
+        <table>
+          <tr>
+            <th>Address</th>
+            <th>Phone Number</th>
+            <th>Email Address</th>
+          </tr>
+          <tr>
+            <td>{{ brewery.brewery_contact.brewery_address }}</td>
+            <td>{{ brewery.brewery_contact.phone }}</td>
+            <td>{{ brewery.brewery_contact.email }}</td>
+          </tr>
+        </table>
+      </div>
+
+      <div class="hours">
+        <label>Hours of Operation</label>
+        <table id="week">
+          <tr>
+            <th>Day</th>
+            <th>Open</th>
+            <th>Close</th>
+          </tr>
+          <tr>
+            <td>Sunday</td>
+            <td>{{ this.convertTime(brewery.brewery_hours.sunday_open) }}</td>
+            <td>{{ this.convertTime(brewery.brewery_hours.sunday_close) }}</td>
+          </tr>
+          <tr>
+            <td>Monday</td>
+            <td>{{ this.convertTime(brewery.brewery_hours.monday_open) }}</td>
+            <td>{{ this.convertTime(brewery.brewery_hours.monday_close) }}</td>
+          </tr>
+          <tr>
+            <td>Tuesday</td>
+            <td>{{ this.convertTime(brewery.brewery_hours.tuesday_open) }}</td>
+            <td>{{ this.convertTime(brewery.brewery_hours.tuesday_close) }}</td>
+          </tr>
+          <tr>
+            <td>Wednesday</td>
+            <td>{{ this.convertTime(brewery.brewery_hours.wednesday_open) }}</td>
+            <td>{{ this.convertTime(brewery.brewery_hours.wednesday_close) }}</td>
+          </tr>
+          <tr>
+            <td>Thursday</td>
+            <td>{{ this.convertTime(brewery.brewery_hours.thursday_open) }}</td>
+            <td>{{ this.convertTime(brewery.brewery_hours.thursday_close) }}</td>
+          </tr>
+          <tr>
+            <td>Friday</td>
+            <td>{{ this.convertTime(brewery.brewery_hours.friday_open) }}</td>
+            <td>{{ this.convertTime(brewery.brewery_hours.friday_close) }}</td>
+          </tr>
+          <tr>
+            <td>Saturday</td>
+            <td>{{ this.convertTime(brewery.brewery_hours.saturday_open) }}</td>
+            <td>{{ this.convertTime(brewery.brewery_hours.saturday_close) }}</td>
+          </tr>
+        </table>
+      </div>
+
+      <div class="beers">
+          <button class="btn-see-beers" v-on:click="$router.push({ name: 'BeerListView', params: { breweryId: brewery.brewery_id }})">See Beers</button>
+      </div>
+
+      <div class="edit">
+          <button class="btn-edit-contact" v-on:click="editContact">Edit Contact Info</button>
+      </div>
+
     </div>
   </div>
 </template>
@@ -174,9 +130,8 @@
   }
 </script>
   
-
-  
 <style>
+
 .BreweryDetails{
   margin-left: 12px;
   margin-right: 12px;
@@ -195,7 +150,10 @@
 
 }
 
-
+#two {
+ display: flex;
+ flex-direction: column;
+}
 
 </style>
   
