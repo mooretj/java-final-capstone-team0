59c6b15a632087eb490314ebadbdbb86c69ac350
TJ Moore <mooreth4791@gmail.com>
2023-12-14 14:29:01 -0500

    changes

--- vue/src/components/ReviewForm.vue
+++ vue/src/components/ReviewForm.vue
@@ -91,7 +91,7 @@
                   // this.$router.push({name: "BeerDetailsView", params: {beerId: this.$route.params.beerId}});
                 // }
                 this.isHidden = false;
-                this.$router.go(0);
+                // this.$router.go(0);
               })
               .catch(error => {
                 this.handleErrorResponse(error, 'adding');
